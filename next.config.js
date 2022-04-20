3f577df7592247192a1afcaabcc9c03e40a9b877
Adam Wilder <wilderav86@gmail.com>
2022-04-20 11:21:14 -0700

    optimize fonts set to true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  optimizeFonts: false,
+  // optimizeFonts: false,
   env: {
     NEXT_PUBLIC_INSTA_ID: process.env.NEXT_PUBLIC_INSTA_ID,
     NEXT_PUBLIC_INSTA_KEY: process.env.NEXT_PUBLIC_INSTA_KEY,
