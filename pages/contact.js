9e9b876172c62d06c8d2313637f8889029cfd162
Adam Wilder <wilderav86@gmail.com>
2022-04-28 11:00:25 -0700

    removed contact form component

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,9 @@
 import PageTransition from "../animations/PageTransition";
 import ContactForm from "../components/Form/ContactForm";
 import styles from "../styles/Contact.module.css";
+import { Form, Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   const tripOptions = [
@@ -40,7 +43,136 @@ const Contact = () => {
                 <p className={styles.accent}>(904) 248-9257</p>
               </div>
             </div>
-            <ContactForm />
+            <div className={styles.form}>
+              <Form
+                name="contact-v1"
+                method="post"
+                data-netlify="true"
+                data-netlify-recaptcha="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit="submit"
+              >
+                <input type="hidden" name="contact-v1" value="contact" />
+                <input type="hidden" name="bot-field" />
+                <Row>
+                  <Col>
+                    <Form.Group className={styles.first} controlId="First name">
+                      <Form.Label className={styles.label}>
+                        First name
+                      </Form.Label>
+                      <Form.Control required type="text" name="First name" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className={styles.last} controlId="Last-name">
+                      <Form.Label className={styles.label}>
+                        Last name
+                      </Form.Label>
+                      <Form.Control required type="text" name="Last name" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label className={styles.label}>
+                        Email address
+                      </Form.Label>
+                      <Form.Control required type="email" name="email" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className={styles.phone} controlId="Phone">
+                      <Form.Label className={styles.label}>
+                        Phone number
+                      </Form.Label>
+                      <Form.Control name="phone" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Form.Group className="mb-3" controlId="Message">
+                    <Form.Label className={styles.label}>Message</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows="3"
+                      name="trip info"
+                      placeholder="Tell us more about the adventure you'd like to take, as well as
+          your preferred dates."
+                    />
+                  </Form.Group>
+                </Row>
+                <Row>
+                  <Form.Group className="mb-3" controlId="tripDropDown">
+                    <Form.Select
+                      id="trip option"
+                      name="Trip option"
+                      defaultValue=""
+                    >
+                      <option value="" disabled>
+                        Choose your trip
+                      </option>
+                      {tripOptions.map((option, id) => (
+                        <option value={option} key={`trip${id}`}>
+                          {option}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
+                <Row>
+                  <Form.Group className="mb-3" controlId="fishDropDown">
+                    <Form.Select
+                      id="Fish option"
+                      name="Fish option"
+                      defaultValue=""
+                    >
+                      <option value="" disabled>
+                        Choose your fish
+                      </option>
+                      {fishOptions.map((option, id) => (
+                        <option value={option} key={`fish${id}`}>
+                          {option}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
+                <Row>
+                  <Form.Group className="mb-3" controlId="waterDropDown">
+                    <Form.Select
+                      id="water option"
+                      name="Water option"
+                      defaultValue=""
+                    >
+                      <option value="" disabled>
+                        Choose your water
+                      </option>
+                      {waterOptions.map((option, id) => (
+                        <option value={option} key={`water${id}`}>
+                          {option}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
+                <Row>
+                  <div className={styles.btn}>
+                    <Button
+                      as={motion.button}
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      size="lg"
+                      variant="custom"
+                      type="submit"
+                    >
+                      Send
+                    </Button>
+                  </div>
+                </Row>
+              </Form>
+            </div>
+            {/* <ContactForm /> */}
           </div>
         </div>
       </PageTransition>
